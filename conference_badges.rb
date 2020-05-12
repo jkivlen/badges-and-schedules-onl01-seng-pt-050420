ecbd73c793407a6d556e3cdc49719b5b66b9de59
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-05-12 17:49:20 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,4 +3,24 @@
 attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def badge_maker(name)
-  
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  newarray = []
+  array.each do |name|
+    newarray.push("Hello, my name is #{name}.")
+  end
+  return newarray
+end
+
+def assign_rooms(array)
+  newarray = []
+  counter = 1
+  array.each do |name|
+    newarray.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+    counter += 1
+  end
+  return newarray
+end
+
