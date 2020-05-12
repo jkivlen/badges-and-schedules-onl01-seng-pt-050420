c02a106a00761679be4a4454eab89c84d3bfeafd
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-05-12 17:38:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,7 @@
 # Write your code here.
 Def badge_maker(name)
 
-name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 name.each do |attendee|
   puts "Hello, my name is #{attendee}"
