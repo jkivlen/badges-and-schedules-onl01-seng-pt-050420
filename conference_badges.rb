925e3d4d89524e7dd316d3590f111276671be31e
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-05-12 17:33:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,4 @@
 # Write your code here.
 Def badge_maker(name)
-attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
